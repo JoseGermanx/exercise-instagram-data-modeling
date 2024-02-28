34c03a3d8a5c90df5eeb2e496b8907023bfe5d73
Jose Germán Martínez <jgermanmm@gmail.com>
2024-02-28 01:41:44 +0000

    Add models for Instagram database

--- src/models.py
+++ src/models.py
@@ -7,26 +7,62 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
+# modelos para recrear la base de datos de instagram
+
+class User(Base):
+    __tablename__ = 'user'
+    # Here we define columns for the table user
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+    lastname = Column(String(250), nullable=False)
+    username = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+    password = Column(String(250), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
+class Post(Base):
+    __tablename__ = 'post'
+    # Here we define columns for the table post
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    image = Column(String(250), nullable=False)
+    description = Column(String(250), nullable=False)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
 
-    def to_dict(self):
-        return {}
+class Comment(Base):
+    __tablename__ = 'comment'
+    # Here we define columns for the table comment
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    comment = Column(String(250), nullable=False)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+    post_id = Column(Integer, ForeignKey('post.id'))
+    post = relationship(Post)
+
+class Like(Base):
+    __tablename__ = 'like'
+    # Here we define columns for the table like
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+    post_id = Column(Integer, ForeignKey('post.id'))
+    post = relationship(Post)
+
+class Follow(Base):
+    __tablename__ = 'follow'
+    # Here we define columns for the table follow
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+    follower_id = Column(Integer, ForeignKey('user.id'))
+    follower = relationship(User)
+
+    # def to_dict(self):
+    #     return {}
 
 ## Draw from SQLAlchemy base
 try:
